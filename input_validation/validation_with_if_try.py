4ba6aaac23a36640915d43bc2270ff937ab27107
Ryan Blankenship <ry_blank@hotmail.com>
2019-09-17 17:04:04 -0500

    Fourth commit

--- input_validation/validation_with_if_try.py
+++ input_validation/validation_with_if_try.py
@@ -11,21 +11,25 @@ Update: Function now taking parameters.
 
 
 def average(score1, score2, score3):
-    if score1 <= 0 or score2 <= 0:
+    if score1 <= 0 or score2 <= 0 or score3 <= 0:
         raise ValueError
     else:
-        average_score = (float(score1) + float(score2) + float(score3)) / 3
+        average_score = float(score1 + score2 + score3) / 3
         return average_score
 
 
 if __name__ == '__main__':
-    last_name = input("Please enter your last name:")
-    first_name = input("Please enter your first name:")
-    age = input("Please enter your age:")
-    score_one = int(input("Please enter your first test score:"))
-    score_two = int(input("Please enter your second test score:"))
-    score_three = int(input("Please enter your third test score:"))
-    average_scores = average(score_one, score_two, score_three)
-    print(last_name + ',' + first_name + ' ' + 'age:' + age + ' ' +
+    try:
+        last_name = input("Please enter your last name:")
+        first_name = input("Please enter your first name:")
+        age = input("Please enter your age:")
+        score_one = int(input("Please enter your first test score:"))
+        score_two = int(input("Please enter your second test score:"))
+        score_three = int(input("Please enter your third test score:"))
+        average_scores = average(score_one, score_two, score_three)
+    except ValueError:
+        print("Unable to process negative scores.")
+    else
+        print(last_name + ',' + first_name + ' ' + 'age:' + age + ' ' +
           'average score: ' + '%5.2f' % average_scores)
     # the expected output is user input of 3 test scores then displaying the average.

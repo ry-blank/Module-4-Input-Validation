b79f3aa2839272cde2c1662dd98f773295ee80ff
Ryan Blankenship <ry_blank@hotmail.com>
2019-09-16 22:13:39 -0500

    Initial commit

--- input_validation/validation_with_if.py
+++ input_validation/validation_with_if.py
@@ -8,16 +8,18 @@ the average of the 3 test scores.
 """
 
 
-def average():
-    score1 = int(input("Please enter your first test score:"))
-    score2 = int(input("Please enter your second test score:"))
-    score3 = int(input("Please enter your third test score:"))
+def average(score1, score2, score3):
+    average_score = (int(score1) + int(score2) + int(score3)) / 3
+    return average_score
 
 
 if __name__ == '__main__':
     last_name = input("Please enter your last name:")
     first_name = input("Please enter your first name:")
     age = input("Please enter your age:")
+    score1 = int(input("Please enter your first test score:"))
+    score2 = int(input("Please enter your second test score:"))
+    score3 = int(input("Please enter your third test score:"))
     average_score = average()
     print(last_name + ',' + first_name + ' ' + 'age:' + age + ' ' +
           'average score: ' + '%5.2f' % average_score)

efe79beb16c4319326414cea99b6c004d0c60c21
Ryan Blankenship <ry_blank@hotmail.com>
2019-09-17 16:50:07 -0500

    Initial Commit

---
+++ input_validation/validation_with_if_try.py
@@ -0,0 +1,28 @@
+"""
+Program: Basic Input and Format Output Program - Module 3
+Author: Ryan Blankenship
+Last date modified: 09/15/2019
+The purpose of this program is to take tests scores,
+in this 3 entered by the user. It will then return
+the average of the 3 test scores.
+
+Update: Function now taking parameters.
+"""
+
+
+def average(score1, score2, score3):
+    average_score = float(score1 + score2 + score3) / 3
+    return average_score
+
+
+if __name__ == '__main__':
+    last_name = input("Please enter your last name:")
+    first_name = input("Please enter your first name:")
+    age = input("Please enter your age:")
+    score_one = int(input("Please enter your first test score:"))
+    score_two = int(input("Please enter your second test score:"))
+    score_three = int(input("Please enter your third test score:"))
+    average_scores = average(score_one, score_two, score_three)
+    print(last_name + ',' + first_name + ' ' + 'age:' + age + ' ' +
+          'average score: ' + '%5.2f' % average_scores)
+    # the expected output is user input of 3 test scores then displaying the average.
---
+++ test/test_validation_with_try.py
@@ -0,0 +1,26 @@
+import unittest
+import unittest.mock as mock
+from input_validation import validation_with_if_try
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_average_equal(self):
+        with mock.patch('builtins.input', side_effect=[85, 90, 95]):
+            assert validation_with_if_try.average(85, 90, 95) == 90
+
+    def test_average_not_equal_(self):
+        with mock.patch('builtins.input', side_effect=[85, 90, 95]):
+            assert validation_with_if_try.average(85, 90, 95) != 91
+
+    def test_average_greater_than(self):
+        with mock.patch('builtins.input', side_effect=[85, 90, 95]):
+            assert validation_with_if_try.average(85, 90, 95) > 89
+
+    def test_average_negative_input(self):
+        with self.assertRaises(ValueError):
+            validation_with_if_try.average(-85, 90, 95)
+
+
+if __name__ == '__main__':
+    unittest.main()
